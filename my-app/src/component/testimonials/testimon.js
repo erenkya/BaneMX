ae7c6eb6ccde10c4230765465ad483bcdfc142d8
Eren Kaya <115587114+erenkya@users.noreply.github.com>
2023-08-23 20:04:35 +0300

    Add files via upload

---
+++ my-app/src/component/testimonials/testimon.js
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./testimon.module.css";
+
+const testimon = () => {
+  return <div>Testimonial </div>;
+};
+
+export default testimon;
--- my-app/src/component/testimonials/testimonial.js
+++ my-app/src/component/testimonials/testimonial.js
@@ -1,11 +1,17 @@
 import React from "react";
+import Testimon from "./testimon";
 import style from "./testimonial.module.css";
 
 const testimonial = () => {
   return (
     <div className={style.container}>
-      <div className={style.testimonialContainer}></div>
-      <div className={style.backgroundImage}></div>
+      <div className={style.testimonialContainer}>
+        <Testimon image="" name="" comment="" score="" />
+        <Testimon image="" name="" comment="" score="" />
+        <Testimon image="" name="" comment="" score="" />
+        <Testimon image="" name="" comment="" score="" />
+      </div>
+      <div className={style.backgroundImage}>Photo</div>
     </div>
   );
 };
